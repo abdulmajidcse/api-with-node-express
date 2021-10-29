36c61b2c3633b822be20274bbf287d364e64eb57
Abdul Majid <abdulmajidcse@gmail.com>
2021-10-29 10:04:19 +0600

    parse request body

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const configApp = require('./config/app');
+const configApp = require("./config/app");
 
-app.get('/', function(req, res) {
-    res.send(`Welcome to ${configApp.name}`);
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }));
+// parse application/json
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send(`Welcome to ${configApp.name}`);
 });
 
-app.listen(configApp.port, function() {
-    console.log(`App listening at http://localhost:${configApp.port}`);
+app.post("/todos", function (req, res) {
+  const response = {
+    message: "Todo Created!",
+    data: req.body,
+    success: true,
+  };
+
+  res.send(response);
+});
+
+app.listen(configApp.port, function () {
+  console.log(`App listening at http://localhost:${configApp.port}`);
 });
