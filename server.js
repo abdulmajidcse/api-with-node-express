a3177c1f94013fb6b82206d25991c2c4a075fd59
Abdul Majid <abdulmajidcse@gmail.com>
2021-10-30 10:40:31 +0600

    todo validation

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const configApp = require("./config/app");
-const { body, validationResult } = require("express-validator");
+const { body, validationResult, checkSchema } = require("express-validator");
 
 // parse application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }));
@@ -14,8 +14,30 @@ app.get("/", function (req, res) {
 
 app.post(
   "/todos",
-  body("title").notEmpty().isLength({ min: 3 }),
-  body("details").notEmpty().isLength({ min: 5 }),
+  // body("title", "Required").notEmpty().withMessage('The title field is required.').isLength({ min: 5 }).withMessage('min character 5.'),
+  // body("details").notEmpty().isLength({ min: 5 }),
+  checkSchema({
+    title: {
+      notEmpty: true,
+      errorMessage: "title is required.",
+      isLength: {
+        options: {
+          min: 3
+        },
+        errorMessage: "title is require minimum 3 charater."
+      },
+    },
+    details: {
+      notEmpty: true,
+      errorMessage: "details is required.",
+      isLength: {
+        options: {
+          min: 5
+        },
+        errorMessage: "details is require minimum 5 charater."
+      },
+    }
+  }),
   function (req, res) {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
